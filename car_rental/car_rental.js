dd1893b2d0c2c51e20d4df5421ffdaf217422130
shivampatel0106 <122294017+shivampatel0106@users.noreply.github.com>
2023-04-04 23:25:36 -0400

    Add files via upload

---
+++ car_rental/car_rental.js
@@ -0,0 +1,19 @@
+//scrollhover
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal('.text',{delay:200, origin: 'top'})
+sr.reveal('.form-container form',{delay:800, origin: 'left'})
+sr.reveal('.heading',{delay:400, origin: 'top'})
+sr.reveal('.ride-container .box',{delay:400, origin: 'top'})
+sr.reveal('.accessories-container .box',{delay:400, origin: 'top'})
+sr.reveal('.services-container .box',{delay:400, origin: 'top'})
+sr.reveal('.about-container about',{delay:400, origin: 'top'})
+sr.reveal('.reviews-container',{delay:400, origin: 'top'})
+sr.reveal('.newsletter-container',{delay:400, origin: 'top'})
+
+
