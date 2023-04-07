97fb3e557de60583d2e734120481178cef06d0f8
shivampatel0106 <122294017+shivampatel0106@users.noreply.github.com>
2023-04-07 18:42:05 -0400

    Update car_rental.js

--- car_rental/car_rental.js
+++ car_rental/car_rental.js
@@ -16,4 +16,15 @@ sr.reveal('.about-container about',{delay:400, origin: 'top'})
 sr.reveal('.reviews-container',{delay:400, origin: 'top'})
 sr.reveal('.newsletter-container',{delay:400, origin: 'top'})
 
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
